55de77b051df51e1e18b92467f444db5ac689b1b
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-11-13 09:40:03 -0300

    Aplicativo gerador de frases com banco de dados completo

--- src/main/java/br/com/alura/MoviePhrasesApp/Principal/Principal.java
+++ src/main/java/br/com/alura/MoviePhrasesApp/Principal/Principal.java
@@ -5,18 +5,29 @@ import br.com.alura.MoviePhrasesApp.repository.FraseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
 
     private FraseRepository repositorio;
+    private Scanner leitura = new Scanner(System.in);
 
     public Principal(FraseRepository repositorio) {
         this.repositorio = repositorio;
     }
 
     public void exibeMenu(){
-        List<Frase> listaFrases = repositorio.findAll();
-        listaFrases.stream().forEach(System.out::println);
+        var option = -1;
+        while (option != 1) {
+
+        Frase fraseAleatoria = repositorio.fraseAleatoria();
+        System.out.println(fraseAleatoria);
+            System.out.println("Quer continuar ? (Digite 1 para sair)");
+        option = leitura.nextInt();
+        leitura.nextLine();
+
+        }
+
     }
 
 }
--- src/main/java/br/com/alura/MoviePhrasesApp/repository/FraseRepository.java
+++ src/main/java/br/com/alura/MoviePhrasesApp/repository/FraseRepository.java
@@ -2,9 +2,11 @@ package br.com.alura.MoviePhrasesApp.repository;
 
 import br.com.alura.MoviePhrasesApp.Model.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
 
-
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    Frase fraseAleatoria();
 
 }
